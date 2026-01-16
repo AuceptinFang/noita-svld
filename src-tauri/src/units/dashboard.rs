e97233580baf758966c4587c2938f9b3367bd979
Auceptin <wandererklein@icloud.com>
2026-01-16 13:26:05 +0800

    1.16.1

--- src-tauri/src/units/dashboard.rs
+++ src-tauri/src/units/dashboard.rs
@@ -1,36 +1,48 @@
 use std::fs;
+use crate::db;
 use serde::Serialize;
 use walkdir::WalkDir;
 use std::process::Command;
-use log::debug;
+use log::{debug, error};
+use crate::backup::commands::get_all_backups;
+use crate::db::Db;
 use crate::units::path;
 
 #[derive(Serialize, Debug)]
 pub struct DashboardStats {
     backup_count: usize,
-    total_size: u64,    // 单位字节
+    total_size: i64,    // 单位字节
     is_ready: bool,     // 后端健康
 }
 
 #[tauri::command]
 pub async fn get_dashboard_stats() -> Result<DashboardStats, String> {
-    let mut total_size : u64 = 0;
+    let mut total_size : i64 = 0;
     let is_ready : bool = true;
 
     let backup_root = path::get_data_path()?;
-    // 计算文件夹数量
-    let count = fs::read_dir(&backup_root)
-        .map_err(|e| e.to_string())?
-        .count();
 
-    // 递归计算总大小
-    total_size = WalkDir::new(&backup_root)
-        .into_iter()
-        .filter_map(|entry| entry.ok())
-        .filter_map(|entry| entry.metadata().ok())
-        .filter(|metadata| metadata.is_file())
-        .map(|m| m.len())
-        .sum();
+    let db_path = "./db/backups.db".to_string();
+    let mut conn = match Db::new(db_path).await{
+        Ok(db) => db,
+        Err(e) => {
+            error!("建立数据库连接出错: {}",e);
+            return Err(e.to_string());
+        }
+    };
+
+    let backups = db::Db::get_all_backup(&mut conn).await.map_err(|e| {
+        error!("查询存档错误");
+        "查询数据库失败".to_string()
+    })?;
+    // 计算文件夹数量
+    let count = backups.len();
+
+    // 计算总大小
+    for backup in backups {
+        total_size += backup.size;
+    }
+
     debug!("存档数:{} ，总大小：{}", total_size, count);
 
     Ok(
